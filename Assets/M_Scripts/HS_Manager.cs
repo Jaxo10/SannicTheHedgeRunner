148ff7bcb07975bdb7f8c3957e80cea6687d76cf
Oeldin <leodintner@yahoo.de>
2014-10-15 00:37:15 +0200

    HighScore
    Added API wrapper class

---
+++ Assets/M_Scripts/HS_Manager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using Newtonsoft.Json;
+using System.Net;
+using System.IO;
+
+public class HS_Manager : MonoBehaviour {
+
+	HS_Object geths () {
+        WebRequest request = WebRequest.Create("http://motes.at/hrunnerhs.php?action=geths");
+        WebResponse response = request.GetResponse();
+        Stream dataStream = response.GetResponseStream();
+        StreamReader reader = new StreamReader(dataStream);
+        string responseFromServer = reader.ReadToEnd();
+
+        HS_Object hslist = JsonConvert.DeserializeObject<HS_Object>(responseFromServer);
+
+        reader.Close();
+        response.Close();
+        return hslist;
+	}
+	
+	void seths (string name, int score) {
+        WebRequest request = WebRequest.Create("http://motes.at/hrunnerhs.php?action=seths&name="+name+"&score="+score.ToString());
+        WebResponse response = request.GetResponse();
+        response.Close();
+	}
+}
---
+++ Assets/M_Scripts/HS_Object.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HS_Object : MonoBehaviour {
+
+    public List<Score> Scores { get; set; }
+
+
+}
+
+public class Score
+{
+    public string score { get; set; }
+    public string name { get; set; }
+}
