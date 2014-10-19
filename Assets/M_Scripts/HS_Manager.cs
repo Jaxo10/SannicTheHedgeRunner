385c4454577474f0520c18ac58ec04ed2d009e9d
Oeldin <leodintner@yahoo.de>
2014-10-19 17:34:53 +0200

    Deleted old unused code

--- Assets/M_Scripts/HS_Manager.cs
+++ Assets/M_Scripts/HS_Manager.cs
@@ -1,29 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
-//using Newtonsoft.Json;
-//using System.Net;
-//using System.IO;
 using System.Text;
 using MiniJSON;
 using System.Collections.Generic;
-//using System;
-
-//#if !UNITY_EDITOR && UNITY_METRO
 using UnityEngine.Windows;
-/*#else
-using System.Security.Cryptography;
-#endif*/
+
 
 public class HS_Manager : MonoBehaviour {
 
 	public HS_Object geths () {
-        /*WebRequest request = WebRequest.Create("http://motes.at/hrunnerhs.php?action=geths");
-        WebResponse response = request.GetResponse();
-        Stream dataStream = response.GetResponseStream();
-        StreamReader reader = new StreamReader(dataStream);
-        string responseFromServer = reader.ReadToEnd();
-        reader.Dispose();
-        response.Close();*/
 
         WWW WWrequest = new WWW("http://motes.at/hrunnerhs.php?action=geths");
         HS_Object hslist = null;
@@ -32,18 +17,6 @@ public class HS_Manager : MonoBehaviour {
         }
         if (WWrequest.isDone)
         {
-            
-/*#if !UNITY_EDITOR && UNITY_METRO
-            hslist = JsonConvert.DeserializeObject<HS_Object>(WWrequest.text);
-#else
-            hslist = new HS_Object();
-            Score sitem = new Score();
-            sitem.name = "Fuck Unity";
-            sitem.score = "404";
-            
-            hslist.Scores = new List<Score>();
-            hslist.Scores.Add(sitem);
-#endif*/
 
             var deserialized = Json.Deserialize(WWrequest.text) as Dictionary<string, object>;
             List<object> TheScores = (List<object>) deserialized["Scores"];
@@ -74,16 +47,11 @@ public class HS_Manager : MonoBehaviour {
         code = ComputeHash(code);
 
         new WWW("http://motes.at/hrunnerhs.php?action=seths&name=" + name + "&score=" + score.ToString() + "&uid=" + UID + "&code=" + code);
-
-        /*WebRequest request = WebRequest.Create("http://motes.at/hrunnerhs.php?action=seths&name="+name+"&score="+score.ToString()+"&uid="+UID+"&code="+code);
-        WebResponse response = request.GetResponse();
-        response.Close();*/
 	}
 
     public string ComputeHash(string s)
     {
 
-//#if !UNITY_EDITOR && UNITY_METRO
         // Form hash
         byte[] hashBytes = Crypto.ComputeMD5Hash(System.Text.Encoding.UTF8.GetBytes(s));
         
@@ -92,16 +60,5 @@ public class HS_Manager : MonoBehaviour {
             sb.Append(b.ToString("x2").ToLower());
         return sb.ToString();
 
-/*#else
-        StringBuilder sb = new StringBuilder();
-        MD5 md5Hasher = MD5.Create();
-        byte[] tempSource = Encoding.ASCII.GetBytes(s);
-
-            foreach (System.Byte b in md5Hasher.ComputeHash(tempSource))
-                sb.Append(b.ToString("x2").ToLower());
-
-            
-        return sb.ToString();
-#endif*/
     }
 }
